4898061a7df1bd73148d01e99aedee8b254d9114
stephencdevaney <stephendevaney@rocketmail.com>
2021-10-13 20:38:27 -0500

    worked on main function for scanner

--- main.c
+++ main.c
@@ -1,7 +1,29 @@
 #include "queue.h"
+#include <string.h>
+
 
 int main(int argc, char **argv){
+    FILE *inFile;
+    char curToken[20];
+    inFile = fopen("tokenFile1.txt", "r");
+    if(inFile == NULL)
+    {
+        printf("File does not exist!!!");
+        exit(1);
+    }
     queue_t *tokqueue;
     tokqueue = newQueue();
-	return 0;
+    int c;
+    c = fgetc(inFile);
+    while(c != EOF){
+        printf("%c", (char)c);
+        printf("/n");
+        //ungetc(c, inFile);
+        if(strcmp(curToken, "error")){
+            printf("%s", curToken);
+            exit(1);
+        }
+        c = fgetc(inFile);
+    }
+	exit(0);
 }
